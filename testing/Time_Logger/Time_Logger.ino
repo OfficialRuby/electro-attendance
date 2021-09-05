da2dce309d7741e5c8421b9a8f687bf992523035
Arduino Development <arduino@ruby.local>
2021-09-05 06:13:45 +0100

    got SD working

--- testing/Time_Logger/Time_Logger.ino
+++ testing/Time_Logger/Time_Logger.ino
@@ -1,14 +1,38 @@
 // Libraries for SD card
-#include "FS.h"
-#include "SD.h"
+#include <FS.h>
+#include <SD.h>
 #include <SPI.h>
 // Libraries to get time from NTP Server
-#include <WiFi.h>
+#include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-
+String formattedDate, dayStamp, ID,dateStamp,fullname,timeStamp;
 #define SD_CS  D8
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP,"pool.ntp.org");
+
+const char *ssid     = "LAB WLAN";
+const char *password = "qwertyuiop";
+
+
 void setup(){
+     // Connect to Wi-Fi
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+
+    timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
   
 Serial.println("Initializing SD card...");
   if (!SD.begin(SD_CS)) {
@@ -16,13 +40,13 @@ Serial.println("Initializing SD card...");
     return;    // init failed
   }
 
-  // If the data.txt file doesn't exist
+
   // Create a file on the SD card and write the data labels
   File attendance = SD.open("/attendance.csv");
   if(!attendance) {
     Serial.println("File doens't exist");
     Serial.println("Creating file...");
-    writeFile(SD, "/attendance.csv", " ID, Fullname, Date, Time \r\n");
+    writeFile( "/attendance.csv", " ID, Fullname, Date, Time \r\n");
   }
   else {
     Serial.println("File already exists");  
@@ -52,51 +76,52 @@ String fetchDate() {
   int splitT = formattedDate.indexOf("T");
   String dateStamp = formattedDate.substring(0, splitT);
   Serial.println(dayStamp);
-  return dateStamp
+  return dateStamp;
 
 }
 void loop(){
   
 }
-// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
-void appendFile(fs::FS &fs, const char * path, const char * message) {
-  Serial.printf("Appending to file: %s\n", path);
 
-  File file = fs.open(path, FILE_APPEND);
-  if(!file) {
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if(file.print(message)) {
-    Serial.println("Message appended");
-  } else {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
+
 
 void logData() {
   String dataMessage = String(ID) + "," + String(fullname) + "," + String(dateStamp) + "," + 
                 String(timeStamp) + "\r\n";
   Serial.print("Save data: ");
   Serial.println(dataMessage);
-  appendFile("", "/attendance.csv", dataMessage.c_str());
+  writeFile( "/attendance.csv", dataMessage.c_str());
 }
 
 
 
 // Write to the SD card (DON'T MODIFY THIS FUNCTION)
-void writeFile(fs::FS &fs, const char * path, const char * message) {
+//void writeFile(const char * path, const char * message) {
+//  Serial.printf("Writing file: %s\n", path);
+//
+//  File file = SD.open(path, FILE_WRITE);
+//  if(!file) {
+//    Serial.println("Failed to open file for writing");
+//    return;
+//  }
+//  if(file.print(message)) {
+//    Serial.println("File written");
+//  } else {
+//    Serial.println("Write failed");
+//  }
+//  file.close();
+//}
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(const char * path, const char * message) {
   Serial.printf("Writing file: %s\n", path);
 
-  File file = fs.open(path, FILE_WRITE);
-  if(!file) {
-    Serial.println("Failed to open file for writing");
-    return;
+  File file = SD.open(path, FILE_WRITE);
+  if(file) {
+    Serial.println("File logged");
+    file.println(message);
   }
-  if(file.print(message)) {
-    Serial.println("File written");
-  } else {
+  else {
     Serial.println("Write failed");
   }
   file.close();
